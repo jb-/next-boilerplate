852adb34e37db5f5761cb2136a50638b2e8f42b4
JB <jongbin.jb.lee@gmail.com>
2018-04-06 02:45:57 +0900

    react-google-maps example

--- pages/another.js
+++ pages/another.js
@@ -1,11 +1,19 @@
 import {Component} from 'react';
+import MyMap from '../components/MyMap';
 
 export default class Another extends Component  {
   render() {
     return (
       <div>
-         
+         <MyMap isMarkerShown={false}
+          googleMapURL=
+             "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `400px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+          />
       </div>
     )
   }
-}
+};
+
